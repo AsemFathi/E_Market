269a7fd8976e183ea5cf80f88369f2e90ed38827
AsemFathi <62443375+AsemFathi@users.noreply.github.com>
2022-12-03 19:04:24 +0200

    init commit

---
+++ app/src/main/java/com/egtactile/e_market/Login.java
@@ -0,0 +1,87 @@
+package com.egtactile.e_market;
+
+import android.annotation.SuppressLint;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Build;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.WindowInsets;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
+
+public class Login extends AppCompatActivity {
+    EditText Email;
+    EditText Password;
+    Button login;
+    Button SignUp;
+    Button Forget;
+    FirebaseAuth auth;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+        Email =findViewById(R.id.editTextTextEmailAddress);
+        Password =findViewById(R.id.editTextTextPassword);
+        login = findViewById(R.id.lgn_btn);
+        auth = FirebaseAuth.getInstance();
+
+        SignUp = findViewById(R.id.SignUP);
+        Forget = findViewById(R.id.forget_password);
+
+
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String email = Email.getText().toString();
+                String password = Password.getText().toString();
+                if (!email.isEmpty() && !password.isEmpty()) {
+                    auth.signInWithEmailAndPassword(email, password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+                            if (task.isSuccessful()) {
+                                Toast.makeText(Login.this, "Login Done", Toast.LENGTH_SHORT).show();
+                                Intent intent = new Intent(Login.this, Home_activity.class);
+                                startActivity(intent);
+                            } else
+                                Toast.makeText(Login.this, task.getException().getMessage(), Toast.LENGTH_SHORT).show();
+                        }
+                    });
+                }
+            }
+        });
+
+        Forget.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Login.this , forgetPass.class);
+                startActivity(intent);
+            }
+        });
+
+        SignUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Login.this , Register.class);
+                startActivity(intent);
+            }
+        });
+
+    }
+
+}
---
+++ app/src/main/java/com/egtactile/e_market/MainActivity.java
@@ -0,0 +1,36 @@
+package com.egtactile.e_market;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+    Button Login;
+    Button Signup;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Login = findViewById(R.id.Login);
+        Signup = findViewById(R.id.Signup);
+
+        Login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this , Login.class);
+                startActivity(intent);
+            }
+        });
+
+        Signup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, Register.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
