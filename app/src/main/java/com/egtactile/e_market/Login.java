150223d2ab0b70145049b49de958d99da7f48411
AsemFathi <62443375+AsemFathi@users.noreply.github.com>
2022-12-12 16:22:22 +0200

    init commit

--- app/src/main/java/com/egtactile/e_market/Login.java
+++ app/src/main/java/com/egtactile/e_market/Login.java
@@ -11,10 +11,12 @@ import android.os.Build;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Looper;
+import android.text.TextUtils;
 import android.view.MotionEvent;
 import android.view.View;
 import android.view.WindowInsets;
 import android.widget.Button;
+import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.Toast;
 
@@ -22,6 +24,8 @@ import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+
+import io.paperdb.Paper;
 
 
 public class Login extends AppCompatActivity {
@@ -32,6 +36,8 @@ public class Login extends AppCompatActivity {
     Button Forget;
     FirebaseAuth auth;
 
+    CheckBox checkBox;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -40,36 +46,40 @@ public class Login extends AppCompatActivity {
         Password =findViewById(R.id.editTextTextPassword);
         login = findViewById(R.id.lgn_btn);
         auth = FirebaseAuth.getInstance();
+        checkBox = findViewById(R.id.remember);
+
+        Paper.init(this);
 
         SignUp = findViewById(R.id.SignUP);
         Forget = findViewById(R.id.forget_password);
 
+        String eemail = Paper.book().read("UserEmail");
+        String Ppassword = Paper.book().read("UserPassword");
 
+        if (eemail != "" && Ppassword !="")
+        {
+            if (!TextUtils.isEmpty(eemail) && !TextUtils.isEmpty(Ppassword))
+            {
+                //login(eemail , Ppassword);
+                Email.setText(eemail);
+                Password.setText(Ppassword);
+                checkBox.setChecked(true);
+            }
+        }
         login.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+
+
                 String email = Email.getText().toString();
                 String password = Password.getText().toString();
                 if (!email.isEmpty() && !password.isEmpty()) {
-                    auth.signInWithEmailAndPassword(email, password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
-                        @Override
-                        public void onComplete(@NonNull Task<AuthResult> task) {
-                            if (task.isSuccessful()) {
-                                Toast.makeText(Login.this, "Login Done", Toast.LENGTH_SHORT).show();
-                                if (email.equals("asem12@gmail.com"))
-                                {
-                                    Intent intent = new Intent(Login.this , Admin_Page.class);
-                                    startActivity(intent);
-                                }
-                                else
-                                {
-                                    Intent intent = new Intent(Login.this, Home.class);
-                                    startActivity(intent);
-                                }
-                                } else
-                                Toast.makeText(Login.this, task.getException().getMessage(), Toast.LENGTH_LONG).show();
-                        }
-                    });
+                    login(email , password);
+                    if(checkBox.isChecked())
+                    {
+                        Paper.book().write("UserEmail",email);
+                        Paper.book().write("UserPassword",password);
+                    }
                 }
             }
         });
@@ -90,6 +100,29 @@ public class Login extends AppCompatActivity {
             }
         });
 
+    }
+    private void login (String email, String password)
+    {
+        auth.signInWithEmailAndPassword(email, password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if (task.isSuccessful()) {
+                    Toast.makeText(Login.this, "Login Done", Toast.LENGTH_SHORT).show();
+                    if (email.equals("asem12@gmail.com"))
+                    {
+                        Intent intent = new Intent(Login.this , Admin_Page.class);
+                        startActivity(intent);
+                    }
+                    else
+                    {
+                        Intent intent = new Intent(Login.this, Home.class);
+                        startActivity(intent);
+                    }
+                } else
+                    Toast.makeText(Login.this, task.getException().getMessage(), Toast.LENGTH_LONG).show();
+
+            }
+        });
     }
 
 }
--- app/src/main/java/com/egtactile/e_market/MainActivity.java
+++ app/src/main/java/com/egtactile/e_market/MainActivity.java
@@ -1,11 +1,21 @@
 package com.egtactile.e_market;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
+import io.paperdb.Paper;
 
 public class MainActivity extends AppCompatActivity {
     Button Login;
@@ -17,10 +27,11 @@ public class MainActivity extends AppCompatActivity {
         Login = findViewById(R.id.Login);
         Signup = findViewById(R.id.Signup);
 
+        Paper.init(this);
         Login.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(MainActivity.this , Login.class);
+                Intent intent = new Intent(MainActivity.this, Login.class);
                 startActivity(intent);
             }
         });
@@ -32,5 +43,38 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+
     }
+      /*  String email = Paper.book().read("UserEmail");
+        String password = Paper.book().read("UserPassword");
+
+        if (email != "" && password !="")
+        {
+            if (!TextUtils.isEmpty(email) && !TextUtils.isEmpty(password))
+            {
+                login(email , password);
+            }
+        }
+    }
+    private void login (String email, String password)
+    {
+        FirebaseAuth auth = FirebaseAuth.getInstance();
+        auth.signInWithEmailAndPassword(email, password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if (task.isSuccessful()) {
+                    Toast.makeText(MainActivity.this, "Login Done", Toast.LENGTH_SHORT).show();
+                    if (email.equals("asem12@gmail.com")) {
+                        Intent intent = new Intent(MainActivity.this, Admin_Page.class);
+                        startActivity(intent);
+                    } else {
+                        Intent intent = new Intent(MainActivity.this, Home.class);
+                        startActivity(intent);
+                    }
+                } else
+                    Toast.makeText(MainActivity.this, task.getException().getMessage(), Toast.LENGTH_LONG).show();
+
+            }
+        });
+    }*/
 }
